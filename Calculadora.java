859805e0c48875396934dabbeb4d518f00921ec4
Jhoni Davinne <jhoni.davinne@hotmail.com>
2020-02-29 23:36:31 -0300

    Segunda Versão (soma e subtração)

--- Calculadora.java
+++ Calculadora.java
@@ -4,12 +4,20 @@ import javax.swing.*;
 
 public class Calculadora2 {
  public static void main(String[] args){
-	 int a,b;
+	 int a,b,aux;
 	 
-	 a = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro para somar:"));
-	 b = Integer.parseInt(JOptionPane.showInputDialog("Digite outro"));
-	 JOptionPane.showMessageDialog(null,a+" + "+b+" = "+(a+b));
-	 
-	 System.out.println(0);
+	 aux = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o tipo de operação que deseja realizar:\n(1) Soma \n(2)Subtração \n(0)Sair"));
+	
+	 	if(aux == 1){
+			a = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro para somar:"));
+			b = Integer.parseInt(JOptionPane.showInputDialog("Digite outro"));
+			JOptionPane.showMessageDialog(null,a+" + "+b+" = "+(a+b));
+		}else if(aux == 2){
+			a = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro para subtrair:"));
+			b = Integer.parseInt(JOptionPane.showInputDialog("Digite outro"));
+			JOptionPane.showMessageDialog(null,a+" - "+b+" = "+(a-b));
+		}else if(aux == 0){
+		System.out.println(0);
+		}
  }
 }
