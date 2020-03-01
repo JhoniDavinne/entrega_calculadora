bf6500d37a42be41da11ab0192c3f5ec7b30e3e2
Jhoni Davinne <jhoni.davinne@hotmail.com>
2020-02-29 23:53:36 -0300

    Calculadora Concluida

--- Calculadora.java
+++ Calculadora.java
@@ -6,7 +6,7 @@ public class Calculadora2 {
  public static void main(String[] args){
 	 int a,b,aux;
 	 
-	 aux = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o tipo de operação que deseja realizar:\n(1) Soma \n(2)Subtração \n(0)Sair"));
+	 aux = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o tipo de operação que deseja realizar:\n(1) Soma\n(2) Subtração\n(3) Multiplicação\n(4) Divisão\n(0) Sair"));
 	
 	 	if(aux == 1){
 			a = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro para somar:"));
@@ -16,6 +16,14 @@ public class Calculadora2 {
 			a = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro para subtrair:"));
 			b = Integer.parseInt(JOptionPane.showInputDialog("Digite outro"));
 			JOptionPane.showMessageDialog(null,a+" - "+b+" = "+(a-b));
+		}else if(aux == 3){
+			a = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro para multiplicar:"));
+			b = Integer.parseInt(JOptionPane.showInputDialog("Digite outro"));
+			JOptionPane.showMessageDialog(null,a+" * "+b+" = "+(a*b));
+		}else if(aux == 4){
+			a = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro para dividir:"));
+			b = Integer.parseInt(JOptionPane.showInputDialog("Digite outro"));
+			JOptionPane.showMessageDialog(null,a+" / "+b+" = "+(a/b));
 		}else if(aux == 0){
 		System.out.println(0);
 		}
